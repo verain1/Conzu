0cea7a2290c522a566bfcca80126f7ae7b6db77f
Ansh <anshchadha9211@gmail.com>
2021-08-12 01:08:34 +0530

    added basic documentation

--- show_news/views.py
+++ show_news/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from news_aggregator_api.ndtv_scraper import get_ndtv_data
-from news_aggregator_api.toi_scraper import get_toi_data
+from news_aggregator_api.ndtv_scraper import get_ndtv_data # ndtv scraper
+from news_aggregator_api.toi_scraper import get_toi_data # toi scraper
 
 # Create your views here.
 
@@ -8,7 +8,12 @@ from news_aggregator_api.toi_scraper import get_toi_data
 
 
 def show_news(request):
-    data1 = get_ndtv_data()
+    '''line 16 and 17
+    return a list of dcitionaries 
+    each dictionary has a link to the article,source(ex. NDTV) and title
+    check template for reference
+    '''
+    data1 = get_ndtv_data() 
     data2 = get_toi_data()
-    data = data1 + data2
+    data = data1 + data2 
     return render(request,'show_news/show_ndtv.html',{"data":data})
