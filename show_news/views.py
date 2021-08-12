0d2cd408ac91a166176fb3ae5c088e32c202d489
Ansh <anshchadha9211@gmail.com>
2021-08-13 00:39:32 +0530

    added the hindu scraper

--- show_news/views.py
+++ show_news/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from news_aggregator_api.ndtv_scraper import get_ndtv_data # ndtv scraper
 from news_aggregator_api.toi_scraper import get_toi_data # toi scraper
-
+from news_aggregator_api.the_hindu_scraper import get_th_data
 # Create your views here.
 
 
@@ -15,5 +15,6 @@ def show_news(request):
     '''
     data1 = get_ndtv_data() 
     data2 = get_toi_data()
-    data = data1 + data2 
+    data3 = get_th_data()
+    data = data1 + data2 + data3
     return render(request,'show_news/show_news.html',{"data":data})
